157a8a721bc7f18a5f18c7ccc5545bad75a27d07
Pedram Meskoub <63091219+pedrammeskoub@users.noreply.github.com>
2023-01-26 23:09:29 -0500

    "please stop duplicating"

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -15,6 +15,20 @@ import OutlinedCard from './cardComp'
 import TextField from '@mui/material/TextField';
 import { useState } from 'react';
 import React from 'react';
+import { ChatGPTAPIBrowser } from 'chatgpt'
+
+async function example() {
+  // use puppeteer to bypass cloudflare (headful because of captchas)
+  const api = new ChatGPTAPIBrowser({
+    email: 'pedram.meskoubhaghighi@mail.utoronto.ca',
+    password: 'Mishaandkissa1986'
+  })
+
+  await api.initSession()
+
+  const result = await api.sendMessage('Hello World!')
+  console.log(result.response)
+}
 
 function App() {
   const txt= useRef(null)
